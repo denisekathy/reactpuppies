eb24d330fa70bc061f0927f3d1ed0273b127bafb
Denise Onukwue <donukwue1@gmail.com>
2021-04-22 13:52:08 -0400

    react puppies part 1

---
+++ crud-helper.js
@@ -0,0 +1,13 @@
+//Connect to the database; dotenv is where our URL is and .config needs that to run which is why it is second
+require("dotenv").config();
+require("./config/database");
+
+//Require the Mongoose models
+//const User = require("./models/user")
+//const Item = require("./models/item")
+//const Category = require("./models/category")
+//const Order = require("./models/order")
+
+
+//local variables will comee in handy 
+//let u, i , c, o;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+//Always require and configure near the top 
+require("dotenv").config()
+
+//connect to the database
+require("./config/database");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "build")));
+
+//Configure to use port 3001 insetead of 3000 during development to avoide collision with React's dev
+const port = process.env.PORT || 3001;
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
+
+//api routes will go here
+
+//The following "catch all " route (note the *) is necessary
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+  
